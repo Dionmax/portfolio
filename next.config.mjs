8509553fe12dec7cb36dec1e506326ec312f2413
Dionmax F. Nobre <dionmaxfn@gmail.com>
2024-03-06 00:30:18 -0300

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextIntlConfig = withNextIntl();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "standalone",
   reactStrictMode: true,
   images: {
     remotePatterns: [
